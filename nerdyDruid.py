9d61e3fd8380b2f0b134e2a4139e364cbe838036
Nostrum <workthethird@gmail.com>
2015-08-21 18:00:35 +0800

    basic coding

--- nerdyDruid.py
+++ nerdyDruid.py
@@ -1,1 +1,62 @@
+import os, sys
+import argparse
+import logging
 import RPi.GPIO as GPIO
+
+global DB
+global args
+global ARGUDB
+
+ARGUDB = []
+
+def doStuff(tTarget):
+    print "Hi"
+
+def setup_logging(level):
+	global DB
+	DB = logging.getLogger('get_jpnews') #replace
+	DB.setLevel(level)
+	handler = logging.StreamHandler(sys.stdout)
+	handler.setFormatter(logging.Formatter('%(module)s %(levelname)s %(funcName)s| %(message)s'))
+	DB.addHandler(handler)
+
+def verify():
+	global tTarget
+	global args
+	parser = argparse.ArgumentParser(description='A nerdy druid plan')
+	parser.add_argument('-v', '--verbose', dest='verbose', action = 'store_true', default=False, help='Verbose mode')
+	parser.add_argument('query', nargs='*', default=None)
+	parser.add_argument('-d', '--database', dest='database', action = 'store', default='/.nerdyDruid/nerdyDruid.db') #replace
+	args = parser.parse_args()
+	tTarget = ' '.join(args.query)
+	log_level = logging.INFO
+	if args.verbose:
+		log_level = logging.DEBUG
+	if not tTarget:
+		parser.print_help()
+		exit()
+	setup_logging(log_level)
+
+def loadDb():
+	home = expanduser('~')
+	if os.path.isfile(home+args.database) is True:
+		f = open(home+args.database,'r')
+		if f is not None:
+			for line in f :
+				if line != '\n' and line[0] != '#':
+					line = line.rstrip('\n')
+					global ARGUDB
+					ARGUDB.append(line)
+		f.close()
+	else:
+		DB.debug('override file is not exist')
+
+def main():
+	doStuff(tTarget)
+
+
+
+if __name__ == '__main__':
+	verify()
+	loadDb()
+	main()
