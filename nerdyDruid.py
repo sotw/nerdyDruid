c9ceae162ca2e01978835ce9d32ac6d5d7c6c2dd
Nostrum <workthethird@gmail.com>
2015-08-21 18:57:31 +0800

    add gpio test

--- nerdyDruid.py
+++ nerdyDruid.py
@@ -3,12 +3,51 @@ from os.path import expanduser
 import argparse
 import logging
 import RPi.GPIO as GPIO
+import time
 
 global DB
 global args
 global ARGUDB
 
 ARGUDB = []
+
+def signupGPIO():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(31,GPIO.OUT)
+    GPIO.setup(33,GPIO.OUT)
+    GPIO.setup(35,GPIO.OUT)
+    GPIO.setup(37,GPIO.OUT)
+    GPIO.output(31,GPIO.HIGH)
+    GPIO.output(33,GPIO.HIGH)
+    GPIO.output(35,GPIO.HIGH)
+    GPIO.output(37,GPIO.HIGH)
+    print "Now doing some relay test..."
+    print "relay 1 ON"
+    GPIO.output(31,GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(31,GPIO.HIGH)
+    print "relay 2 ON"
+    GPIO.output(33,GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(33,GPIO.HIGH)
+    print "relay 3 ON"
+    GPIO.output(35,GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(35,GPIO.HIGH)
+    print "relay 4 ON"
+    GPIO.output(37,GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(37,GPIO.HIGH)
+    print "now all ON"
+    GPIO.output(31,GPIO.LOW)
+    GPIO.output(33,GPIO.LOW)
+    GPIO.output(35,GPIO.LOW)
+    GPIO.output(37,GPIO.LOW)
+    print "now all OFF"
+    GPIO.output(31,GPIO.HIGH)
+    GPIO.output(33,GPIO.HIGH)
+    GPIO.output(35,GPIO.HIGH)
+    GPIO.output(37,GPIO.HIGH)
 
 def doStuff(tTarget):
     print "Hi"
@@ -53,6 +92,7 @@ def loadDb():
 		DB.debug('override file is not exist')
 
 def main():
+    signupGPIO()
 	doStuff(tTarget)
 
 
